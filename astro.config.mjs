6dee697aa9aa8044677674419602ed9ecd6c056f
Yash Diniz <26359876+yashdiniz@users.noreply.github.com>
2023-09-11 15:49:05 +0530

    :new: Preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()]
+  integrations: [mdx(), preact()]
 });
