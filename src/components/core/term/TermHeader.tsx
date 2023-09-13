85b1c41340e586fbfcf220e0ee94f6340d02e4ba
Yash Diniz <26359876+yashdiniz@users.noreply.github.com>
2023-09-13 10:24:16 +0530

    :new: TS Props

--- src/components/core/term/TermHeader.tsx
+++ src/components/core/term/TermHeader.tsx
@@ -1,16 +1,38 @@
 interface Props {
+  /** 
+   * required: command to display
+   */
   command: string;
+  /** 
+  * optional: arguments to the command
+  */
+  args?: string[];
 }
-export default function TermHeader({ command }: Props) {
+export default function TermHeader({ command, args }: Props) {
+  if (command == 'wget') {
+    let url = `${document.location.host}${document.location.pathname}`
+    command = `wget ${url}`
+  } else {
+    if (args) {
+      command = `${command} ${args.join(' ')}`
+    }
+  }
+  // let maxlen = 42;
+  // if (command.length > maxlen) {
+  //   command = command.slice(0, maxlen) + '…'
+  // }
+
   return (
-    <div>
+    <div style={{
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      whiteSpace: 'nowrap',
+    }}>
       <span style={{
         color: '#00ff00',
         paddingRight: '0.5em',
-      }}>
-        %~&gt;
-      </span>
-      {command == 'cd' ? `cd ${document.location.pathname}` : command}
+      }}>%~&gt;</span>
+      <span>{command}</span>
     </div>
   )
 }
