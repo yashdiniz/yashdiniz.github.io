8e31f1cbe1e54b5f62a6cc99c87993997ac27b93
Yash Diniz <26359876+yashdiniz@users.noreply.github.com>
2025-02-14 22:57:30 +0530

    chore: add GA to page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 
 import solid from "@astrojs/solid-js";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yashdiniz.me',
-  integrations: [mdx(), solid()]
+  integrations: [
+    mdx(), solid(),
+    sitemap(), partytown({
+      config: {
+        forward: ['datalayer.push'],
+      }
+    })
+  ],
 });
