9f9c8104da9275bdf741883ef8c0e3c0397c2426
Yash Diniz <26359876+yashdiniz@users.noreply.github.com>
2025-02-15 19:53:55 +0530

    fix: blog.astro issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,16 @@ import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
 
 import solid from "@astrojs/solid-js";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yashdiniz.me',
   integrations: [
     mdx(), solid(),
-    sitemap(), partytown({
+    partytown({
       config: {
         forward: ['datalayer.push'],
       }
-    })
+    }),
   ],
 });
